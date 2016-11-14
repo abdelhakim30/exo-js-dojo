620f26dc7e3211b2da46e32d0b5e38b7d2e2acf5
abdelhakim30 <rkim-30@hotmail.fr>
2016-11-14 17:15:27 +0100

    Add files via upload

---
+++ exo dojo/script.js
@@ -0,0 +1,44 @@
+// Add Html
+function addHtml(){
+  var edithtml = document.getElementById('txtHtml').value;
+  console.log(edithtml);
+  var view = document.getElementById('view');
+  view.innerHTML = edithtml;
+}
+
+// delete VUE
+
+function deleteVue() {
+  document.getElementById('view').innerHTML = "";
+}
+
+// reset Html
+function resetHtml() {
+
+  document.getElementById('txtHtml').value = "";
+
+}
+// Add JS
+
+function addJs() {
+  var baliseScript = document.createElement('script'),
+    view = document.getElementById('view'),
+    inputJs = document.getElementById('txtJs').value;
+
+  baliseScript.type = "text/javascript";
+  baliseScript.text = inputJs;
+  view.appendChild(baliseScript);
+}
+
+// delete JS
+function deleteJs(){
+  document.getElementById('txtJs').value= "";
+}
+
+
+// reload JS
+
+function reloadJs(){
+  location.reload();
+
+}
